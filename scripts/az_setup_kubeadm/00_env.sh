196b7311b0cef8d34877135e033e6db66d801d2f
Ravan Nannapaneni <ravan.nannapaneni@oliverwyman.com>
2021-05-14 08:02:25 +0800

    Improved some comments

--- scripts/az_setup_kubeadm/00_env.sh
+++ scripts/az_setup_kubeadm/00_env.sh
@@ -4,10 +4,10 @@
 export az_vnet_name=k8_vnet
 export az_subnet_name=k8_subnet
 
-# SSH KEY details
+# SSH key details
 export az_ssh_key=k8_ssh_key
 
-# VM Details
+# VM details
 export az_vm_image=UbuntuLTS
 export az_vm_name_master=k8master
 export az_linux_admin_user=k8user
@@ -16,6 +16,7 @@ export az_vm_max_price=0.02
 export az_vm_priority=Spot
 export az_vm_eviction_policy=Delete
 
+# WORKER details
 export WORKER_NODES=2
 
 export az_vm_name_worker_prefix="worker"
--- scripts/az_setup_kubeadm/00_env_sensitive_template.sh
+++ scripts/az_setup_kubeadm/00_env_sensitive_template.sh
@@ -1,2 +1,3 @@
+# Create a file called 00_env_sensitive.sh and copy the contents and make necessary changes
 export az_subscription=<AZURE SUBSCRIPTION ID>
 
