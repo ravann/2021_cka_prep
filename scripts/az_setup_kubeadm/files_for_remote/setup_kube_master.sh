bea27569a9d575b91b407d2ea927f950a3e8f7b8
Ravan Nannapaneni <ravan.nannapaneni@oliverwyman.com>
2021-05-16 17:17:39 +0800

    Added metrics server to cluster ...

--- scripts/az_setup_kubeadm/files_for_remote/setup_kube_master.sh
+++ scripts/az_setup_kubeadm/files_for_remote/setup_kube_master.sh
@@ -6,6 +6,8 @@ sudo kubeadm token create --print-join-command > /tmp/setup_join.sh
 ### Setup network on the cluster
 sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
+sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
 ### Copy config to root user
 sudo mkdir -p /root/.kube
 sudo cp -p /etc/kubernetes/admin.conf /root/.kube/config
